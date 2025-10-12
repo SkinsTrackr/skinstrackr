fac5e89dc1afe50eaf62052c3b5d93df812ba5ea
jzimr <zimmerjan@outlook.com>
2025-10-12 18:11:03 +0200

    First good commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import LoginPage from './pages/login'
+
+function App(): React.JSX.Element {
+  return (
+    <>
+      <div className="isolate"></div>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
